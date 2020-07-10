75db78dc5b102f7e6f28647d13a40ba2ca8c78e5
Leonardo Christino <leomilho@gmail.com>
2020-07-10 15:45:51 -0300

    Incremented with local file l2 and some file verification methods

--- L3MinioCache/FileL2Cache.py
+++ L3MinioCache/FileL2Cache.py
@@ -5,14 +5,14 @@ import os
 from L3MinioCache.CacheInterface import L2Cache
 
 
-class PickleL2Cache(L2Cache):
-    def __init__(self, storage_path: str, extension: str = ".pickle"):
+class FileL2Cache(L2Cache):
+    def __init__(self, storage_path: str, binary: bool = False):
         self.storage_path = Path(storage_path)
         self.storage_path.parent.mkdir(parents=True, exist_ok=True)
-        self.extension = extension
+        self.binary = "b" if binary else ""
 
     def get_path(self, name: str, abs_path: bool = True):
-        ret = self.storage_path / str(name + self.extension)
+        ret = self.storage_path / name
         if abs_path: ret = os.path.abspath(ret)
         return ret
 
@@ -23,9 +23,12 @@ class PickleL2Cache(L2Cache):
         full_path = self.get_path(name)
         if not os.path.isfile(full_path):
             return None
-        return pickle.load(open(full_path, 'rb'))
+        f = open(full_path, "r" + self.binary)
+        ret = f.read()
+        f.close()
+        return ret
 
-    def dump(self, name: str, data):
-        full_path = self.get_path(name)
-        pickle.dump(data, open(full_path, 'wb'))
-        return full_path
+    def dump(self, name: str, data: str):
+        f = open(self.get_path(name), "w" + self.binary)
+        f.write(data)
+        f.close()
--- L3MinioCache/MinioL3Cache.py
+++ L3MinioCache/MinioL3Cache.py
@@ -2,9 +2,13 @@ from minio import Minio
 from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
                          BucketAlreadyExists, NoSuchKey)
 
+from L3MinioCache.CacheInterface import L3Cache
 
-class MinioL3Cache:
-    def __init__(self, minio_client, bucket_name, location="us-east-1"):
+
+class MinioL3Cache(L3Cache):
+    def __init__(self, minio_client: Minio,
+                 bucket_name: str,
+                 location: str = "us-east-1"):
         self.minio_client = minio_client
         self.bucket_name = bucket_name
         self.location = location
@@ -19,7 +23,18 @@ class MinioL3Cache:
             except ResponseError as err:
                 raise
 
-    def load(self, name):
+    def exists(self, name):
+        try:
+            self.minio_client.get_object(
+                bucket_name=self.bucket_name,
+                object_name=name)
+            return True
+        except NoSuchKey:
+            return False
+        except Exception:
+            raise
+
+    def load(self, name: str):
         try:
             return self.minio_client.get_object(
                 bucket_name=self.bucket_name,
@@ -29,5 +44,16 @@ class MinioL3Cache:
         except Exception:
             raise
 
-    def dump(self, name, local_path):
+    def download(self, name: str, path: str):
+        try:
+            return self.minio_client.fget_object(
+                bucket_name=self.bucket_name,
+                object_name=name,
+                file_path=path)
+        except NoSuchKey:
+            return None
+        except Exception:
+            raise
+
+    def dump(self, name: str, local_path: str):
         self.minio_client.fput_object(self.bucket_name, name, local_path)
